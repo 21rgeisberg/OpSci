6306bcc1a0fc4ce371ac208601c35c8c9d8fc21d
Becky Geisberg <21rgeisberg@gannacademy.org>
2022-02-13 14:00:34 +0200

    c

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print(2 + 2)
+import pixellib
+from pixellib.instance import instance_segmentation
+segment_image = instance_segmentation()
+segment_image.load_model("mask_rcnn_coco.h5")
+segment_image.segmentImage("path_to_image", output_image_name = "output_image_path")
