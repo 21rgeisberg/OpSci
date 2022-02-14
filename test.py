cd2875a8a274004372bb97ff61125145604a36a4
Becky Geisberg <21rgeisberg@gannacademy.org>
2022-02-14 11:02:50 +0200

    c

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import pixellib
-from pixellib.instance import instance_segmentation
+from pixellib.instance import instance_segmentation 
 segment_image = instance_segmentation()
 segment_image.load_model("mask_rcnn_coco.h5")
-segment_image.segmentImage("path_to_image", output_image_name = "output_image_path")
+segment_image.segmentImage("semanticTestPhoto.jpeg", output_image_name = "newImage.jpg")
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.widgets import LassoSelector
+# from matplotlib.path import Path
